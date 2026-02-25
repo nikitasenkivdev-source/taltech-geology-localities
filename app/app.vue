4060c49e4dcfc383c8e445752449abdbbd2293c5
Nikita <nekit533068@gmail.com>
2026-02-25 20:08:10 +0200

    configure Tailwind CSS in Nuxt project

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-blue-600">
+      Tailwind is working 🚀
+    </h1>
   </div>
 </template>
